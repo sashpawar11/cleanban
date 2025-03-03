8ea4295e11882fc1aa0be26b9bb5a0f23cda9e56
sashpawar11 <saisaipawar@gmail.com>
2025-03-03 17:40:15 +0530

    Add LocalStorage Object Sync

--- src/script.js
+++ src/script.js
@@ -16,7 +16,8 @@ class boardObj {
 
 
 class IssueObj {
-    constructor(issueName, issueLabel, issueDueDate) {
+    constructor(issuePriority, issueName, issueLabel, issueDueDate) {
+        this.issuePriority = issuePriority;
         this.issueName = issueName;
         this.issueLabel = issueLabel;
         this.issueDueDate = issueDueDate;
@@ -24,12 +25,12 @@ class IssueObj {
 }
 
 
+// PRORTOTYPES
 
 
 // INITIALIZE //
 
 const boardsContainer = document.querySelector('.container');
-const boards = document.querySelectorAll('.board');
 const todoBoard = document.getElementById('todo-board');
 const adhocBoard = document.getElementById('adhoc-board');
 const issueItems = document.querySelectorAll('.issue-item')
@@ -40,19 +41,35 @@ const createIssueModal = document.getElementById("createIssueModal");
 const btncreateIssueModalClose = document.getElementById("closeCreateIssueModalButton");
 const btncreateIssueModalSubmit = document.getElementById("btnSubmitCRIssue");
 const todoItems = document.getElementById('todo-items');
-const colorPickerBoard = document.querySelectorAll('.board-color-picker');
-
-const boardsData = []
 
 
-const todoBoardObj = new boardObj('todoBoard', 'TODO', 'black');
-const todoBoardObjItem = new IssueObj('Create Kanban Board', 'current-sprint', '02/03/2025');
-todoBoardObj.setBoardIssue(todoBoardObjItem);
-boardsData.push(todoBoardObj);
-console.log(boardsData);
-
+let boardsData = []
 
 loadLocales();
+
+
+// fallback/new session
+if (boardsData.length == 0) {
+    const todoBoardObj = new boardObj('todo-board', 'TODO', 'black');
+    const todoBoardObjItem = new IssueObj('P1','Create Kanban Board', 'current-sprint', '03/05/2025');
+    const todoBoardObjItem2 = new IssueObj('P2','Edit and Delete Issue', 'current-sprint', '03/05/2025')
+    const todoBoardObjItem3 = new IssueObj('P3','Sync LocalStorage', 'current-sprint', '03/05/2025');
+    todoBoardObj.setBoardIssue(todoBoardObjItem);
+    todoBoardObj.setBoardIssue(todoBoardObjItem2);
+    todoBoardObj.setBoardIssue(todoBoardObjItem3);
+    boardsData.push(todoBoardObj);
+
+    const inprogressBoardObj = new boardObj('inprogress-board', 'IN-PROGRESS', 'black');
+    boardsData.push(inprogressBoardObj);
+
+    const completedBoardObj = new boardObj('completed-board', 'COMPLETED', 'black');
+    boardsData.push(completedBoardObj);
+
+    syncLocalStorage();
+}
+
+renderBoardData()
+
 
 
 /// EVENT HANDLERS ///////////
@@ -62,6 +79,7 @@ kanbanBoardName.addEventListener('input', () => {
 })
 issueItems.forEach(attachDragHandlers);
 
+const boards = document.querySelectorAll('.board');
 boards.forEach((board) => {
     board.addEventListener('dragover', () => {
         console.log('Something Dragged');
@@ -76,6 +94,7 @@ boards.forEach((board) => {
     })
 })
 
+const colorPickerBoard = document.querySelectorAll('.board-color-picker');
 colorPickerBoard.forEach((picker) => {
     picker.addEventListener('input', updateBoardColor);
     // picker.addEventListener('input', () => {
@@ -192,6 +211,7 @@ function createNewIssue() {
     alert('Issue Created Successfully!');
 }
 
+
 function createNewBoard() {
     const boardDiv = document.createElement('div');
     boardDiv.className = 'board';
@@ -203,11 +223,12 @@ function createNewBoard() {
         const issueItem = document.querySelector('.flying');
         boardDiv.appendChild(issueItem);
     })
-
-    boardsContainer.removeChild(adhocBoard);
     boardsContainer.appendChild(boardDiv);
-    boardsContainer.appendChild(adhocBoard);
+    repositionAdHocBoard();
+
+
 }
+
 
 
 
@@ -229,8 +250,130 @@ function loadLocales() {
         kanbanBoardName.value = boardname;
     }
 
+    loadBoardsDataFromStorage()
+
 
 }
+
+function loadBoardsDataFromStorage() {
+    let loadedBoardData = [];
+    if (localStorage.getItem('boardsData')) {
+        retrievedData = JSON.parse(localStorage.getItem('boardsData')); 
+        boardsData = retrievedData;
+        // boardsData = [...loadedBoardData];
+        console.log('Data Loaded Successfully!',boardsData);
+    };
+}
+
+function syncLocalStorage() {
+    
+    if (boardsData.length > 0) localStorage.setItem('boardsData', JSON.stringify(boardsData));
+
+    console.log('Synced Data! ', JSON.parse(localStorage.getItem('boardsData')));
+}
+
+function renderBoardData() {
+
+ 
+    boardsData.forEach((board) => {
+
+        //boardsContainer
+        if (typeof(board) === 'object') {
+            const tempboard = DOMCreateBoardElement(board)
+            boardsContainer.append(tempboard);
+        }
+       
+    })
+
+    repositionAdHocBoard();
+}
+
+function repositionAdHocBoard() {
+    boardsContainer.removeChild(adhocBoard);
+    boardsContainer.appendChild(adhocBoard);
+}
+
+function DOMCreateBoardElement(board) {
+
+    console.log('insidedomcreateboardelement');
+
+    // boardElement
+    let boardDiv = document.createElement('div');
+    boardDiv.id = board.boardID;
+    boardDiv.className = "board"
+    const boardHeader = document.createElement('div');
+    boardHeader.className = "board-header";
+    const boardTitleSpan = document.createElement('span');
+    boardTitleSpan.innerHTML = board.boardName
+    boardTitleSpan.className = 'board-title';
+    const colorPicker = document.createElement('input');
+    colorPicker.type = 'color';
+    colorPicker.name = 'board-color-picker'
+    colorPicker.className = 'board-color-picker'
+    boardHeader.appendChild(boardTitleSpan);
+    boardHeader.appendChild(colorPicker);
+    boardDiv.appendChild(boardHeader);
+    console.log(boardDiv);
+    boardDiv = DOMCreateIssuesElement(boardDiv,board.boardIssues)
+
+    return boardDiv
+
+}
+
+function DOMCreateIssuesElement(boardDiv, issues) {
+    
+const itemsContainer = document.createElement('div');
+itemsContainer.class = 'items';
+if (boardDiv.id == 'todo-board') itemsContainer.id = 'todo-items';
+
+    issues.forEach((issue) => {
+
+  
+        const issueItemDiv = document.createElement('div');
+        issueItemDiv.draggable = 'true';
+        issueItemDiv.className = 'issue-item';
+        attachDragHandlers(issueItemDiv);
+
+        const prioritySpan = document.createElement('span');
+        prioritySpan.className = 'priority';
+        switch (issue.issuePriority) {
+            case "P1": prioritySpan.classList.add('p1');
+                         break;
+            case "P2": prioritySpan.classList.add('p2');
+                break;
+                case "P3": prioritySpan.classList.add('p3');
+                break;
+            default : prioritySpan.classList.add('p3')
+        }
+
+        issueItemDiv.appendChild(prioritySpan);
+
+        const p = document.createElement('span');
+        p.className = 'issue-content';
+
+        const labelSpan = document.createElement('span');
+        labelSpan.innerText = issue.issueLabel;
+        labelSpan.className = 'issue-label';
+        p.appendChild(labelSpan);
+        const titleSpan = document.createElement('span');
+        titleSpan.innerText = issue.issueName;
+        titleSpan.className = 'issue-title';
+        p.appendChild(titleSpan);
+        const dateSpan = document.createElement('span');
+        dateSpan.innerText = issue.issueDueDate;
+        dateSpan.className = 'issue-date';
+        p.appendChild(dateSpan);
+        attachDragHandlers(p);
+        issueItemDiv.appendChild(p);
+        itemsContainer.appendChild(issueItemDiv);
+    })
+
+    boardDiv.appendChild(itemsContainer);
+    console.log('FinalBoard',boardDiv);
+    return boardDiv;
+
+}
+
 
 
 // // boardObj
